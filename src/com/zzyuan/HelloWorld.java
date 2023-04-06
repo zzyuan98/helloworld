ce8b0488dffe7ce5b3733d0c26b1ad8ef0ef292c
zzyuan <18344163021@163.com>
2023-04-06 16:52:03 +0800

    第一次提交

---
+++ src/com/zzyuan/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.zzyuan;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
---
+++ src/com/zzyuan/Test.java
@@ -0,0 +1,9 @@
+package com.zzyuan;
+
+/**
+ * @author zzy
+ * @create 2023-04-03 16:45
+ */
+public class Test {
+    
+}
